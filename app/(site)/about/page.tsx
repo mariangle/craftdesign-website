c11800920478a3b4e0d33509056ea6b90ee0bb2d
Maria <mariale171001@gmail.com>
2023-06-04 22:39:16 +0200

    add: meta data for about page

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+export const metadata = {
+  title: 'CraftDesign | About Us',
+  description: 'CraftDesign is a passionate team of designers, developers, and digital strategists dedicated to creating exceptional online experiences. With a focus on innovation and attention to detail, we collaborate closely with our clients to bring their visions to life. Learn more about our story, expertise, and how we can help your business thrive in the digital landscape.',
+}
+
 const page = () => {
   return (
-    <div>page</div>
+    <div>Contact Us</div>
   )
 }
 
